ca4d7f52f9b5d0a27b3b0279490298dbd09a6a22
Martin Igarzabal <migarzabal@iiconcepcion.edu.ar>
2025-11-22 05:59:35 -0300

    modifico el modelo

--- backend/models/Concepto.js
+++ backend/models/Concepto.js
@@ -1,23 +1,41 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { sequelize } = require('../config/database');
 
-const Concepto = sequelize.define('Concepto',{
+const Concepto = sequelize.define("Concepto", {
     id: {
         type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+        primaryKey: true,
+        autoIncrement: true
     },
     nombre: {
         type: DataTypes.STRING(100),
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "El nombre no puede estar vacío"
+            },
+            len: {
+                args: [3, 100],
+                msg: "El nombre debe tener entre 3 y 100 caracteres"
+            }
+        }
     },
     descripcion: {
-        type: DataTypes.STRING(255),
-        allowNull: true
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "La descripción no puede estar vacía"
+            },
+            len: {
+                args: [5, 500],
+                msg: "La descripción debe tener entre 5 y 500 caracteres"
+            }
+        }
     }
-},{
+}, {
     tableName: 'conceptos',
-    timestamps:false
+    timestamps: false
 });
 
 module.exports = Concepto;
